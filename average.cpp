a890c4dc67bfa2a806537d78da8cd4f6ce34524f
Sorakrit05 <aomsin22122558@gmail.com>
2025-02-21 13:59:22 +0700

    UD

--- average.cpp
+++ average.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
 
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    vector<double> numbers;
+    for (int i = 1; i < argc; ++i) {
+        numbers.push_back(atof(argv[i]));
+    }
+
+    double sum = 0;
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        sum += numbers[i];
+    }
+    
+    double average = sum / numbers.size();
+    
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << numbers.size() << " numbers = "
+         << fixed << setprecision(3) << average << endl;
+    cout << "---------------------------------" << endl;
+    
+    return 0;
+}
